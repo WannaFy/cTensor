fix: I think there's no need to avoid broadcasting and I replace the mean to sum

ctensor/tensor.py
@@ -22,6 +22,12 @@ def attach_grad(u, grad):
     '''
     During backpropagation, attach computed grad into precedents.
     If forward process includes broadcasting, unbroadcast grad.
+
+
+    change:
+    Frey wong change the grad=grad.mean to grad=grad.sum
+
+    to fix:
     '''
     if not u.requires_grad:
         return
@@ -30,13 +36,18 @@ def attach_grad(u, grad):
         u.grad += grad
         return
 
+    origin_shape=u.grad.shape[:]
+    u.grad.shape=(1,)*(len(grad.shape)-len(u.grad.shape))+origin_shape
     # unbroadcasting
     for dim, chn in enumerate(u.grad.shape):
+        #print("u.grad.shape=",u.grad.shape)
         if chn != grad.shape[dim]:
             assert chn == 1, "Backward unbroadcasting errors"
-            grad = grad.mean(axis=dim, keepdims=True)
+            grad = grad.sum(axis=dim,keepdims=True)
+           # print("grad.shape=",grad.shape)
 
     u.grad += grad
+    u.grad.shape=origin_shape
 
 
 class Tensor:
@@ -236,9 +247,11 @@ class Operator:
         fwd = self.forward(*args)
         if fwd.precedents:
             # Operator in Tensor
+            print("hello\n")
             return Tensor(fwd.data, precedents=[fwd], operator=self)
         else:
             # Operation in NumPy
+            #print("waafuck\n")
             return Tensor(fwd.data, precedents=args, operator=self)
 
     def backward(self, x, precedents):
@@ -253,6 +266,7 @@ class View(Operator):
     def forward(self, x):
         self.origin_shape = x.data.shape
         return Tensor(x.data.reshape(*self.shape))
+
 
     def backward(self, x, precedents):
         u, = precedents

nn_testcase.py
@@ -1,3 +1,4 @@
+from ctensor.functional import relu, sigmoid
 import numpy as np
 from ctensor import Tensor
 import ctensor.functional as F
@@ -23,6 +24,7 @@ Y = Tensor(y.reshape(1000, 1))
 #     def forward(self, x):
 #         return F.sigmoid(self.linear_2(F.leaky_relu(self.linear_1(x))))
 
+
 class Model(nn.Module):
     def __init__(self):
         self.main = nn.Sequential(
@@ -31,12 +33,14 @@ class Model(nn.Module):
             nn.Linear(1000, 2),
             nn.Sigmoid(),
         )
-    
+
     def forward(self, x):
         return self.main(x)
 
+
 model = Model()
 adam = Adam(model.parameters())
+
 
 for _ in range(3000):
     indices = np.random.randint(0, 1000, size=128)
@@ -52,3 +56,28 @@ for _ in range(3000):
 
 pred_y = model(X)
 print(((pred_y.data > 0.5) == Y.data).mean())
+
+a = Tensor(np.array([1]))
+b = Tensor(np.arange(1, 10).reshape(3, 3))
+c = a+b
+d = c.sum()
+d.backward()
+
+a = Tensor(np.array([1, -4, 3]))
+b = Tensor(np.array([3]))
+c = a*b
+c = relu(a*b)
+c.backward()
+print("relu")
+print(c)
+print(c.mean())
+print(c.grad)
+print(b.grad)
+print(a.grad)
+print("sigmoid")
+c = sigmoid(a * (1/b))
+c.backward()
+print(c)
+print(c.grad)
+print(b.grad)
+print(a.grad)